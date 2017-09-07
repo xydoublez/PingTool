84721a9a7b29c00d809ff318cf8207825300ea8e
李志强 <love@lizhiqiang.name>
2017-09-07 17:34:50 +0800

    ..

--- PingTool/Form1.cs
+++ PingTool/Form1.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace PingTool
@@ -14,6 +16,56 @@ namespace PingTool
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(ping);
+            t.IsBackground = true;
+            t.Start();
+        }
+        private void ping()
+        {
+            this.listBox1.BeginInvoke(new Action(() =>
+            {
+                //this.listBox1.BeginUpdate();
+                this.listBox1.Items.Clear();
+                //远程服务器IP
+                string ipStr = this.textBox1.Text.ToString().Trim();
+                //构造Ping实例
+                Ping pingSender = new Ping();
+                //Ping 选项设置
+                PingOptions options = new PingOptions();
+                options.DontFragment = true;
+                //测试数据
+                string data = "test data abcabc";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                //设置超时时间
+                int timeout = 6000;
+                //调用同步 send 方法发送数据,将返回结果保存至PingReply实例
+                while (true)
+                {
+                    PingReply reply = pingSender.Send(ipStr, timeout, buffer, options);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        listBox1.Items.Add(string.Format("{0}  来自 {1} 的回复: 字节={2} 时间={3}ms TTL={4}",
+                            DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"),
+                            reply.Address.ToString(),
+                            reply.Buffer.Length,
+                            reply.RoundtripTime,
+                            reply.Options.Ttl)
+                            );
+
+                    }
+                    else
+                    {
+                        listBox1.Items.Add(string.Format("{0} 失败 状态：{1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"), reply.Status.ToString()));
+                    }
+                    listBox1.Refresh();
+                    Thread.Sleep(1000);
+                }
+            }));
+
         }
     }
 }
